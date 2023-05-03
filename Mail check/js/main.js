4de54353cb0af733d5899808fe299adb2b786c61
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-05-03 17:22:47 +0200

    Replaced prompt with input and add button

--- Mail check/js/main.js
+++ Mail check/js/main.js
@@ -1,28 +1,44 @@
 // Setto le mie variabili globali
 const mailRegistrate = ["mario0@gmail.com", "fabio1@gmail.com", "luca2@gmail.com", "pietro3@gmail.com"];
 let esito = "negativo"; // esito base = mail non presente
+const bottoneControlla = document.getElementById("controlla");
+let messaggioOutput;
 
-// Chiedo a utente propria email
-let miaMail = prompt("Inserisci la tua email");
+// Collego evento a bottone CONTROLLA
+bottoneControlla.addEventListener("click", miaFunzione);
 
-// Riporto in console email inserita dall'untente
-console.log("La tua mail inserita è: " + miaMail);
+// Evento collegato al click del bottone CONTROLLA
+function miaFunzione() {
 
-// creo loop
-for (let i = 0; i < mailRegistrate.length; i++) {
+    // Prendo valore che utente ha scritto nell'input per l'email
+    miaMail = document.getElementById("campoMail").value;
 
-    // Riporto in console elenco mail registrate
-    console.log(mailRegistrate[i]);
+    // Riporto in console email inserita dall'utente
+    console.log("La tua mail inserita è: " + miaMail);
 
-    // SE mail inserita da utente è in elenco mail registrate
-    if (miaMail === mailRegistrate[i]) {
-        esito = "positivo"; // esito diventa POSITIVO
+    // creo loop
+    for (let i = 0; i < mailRegistrate.length; i++) {
+
+        // Riporto in console elenco mail registrate
+        console.log(mailRegistrate[i]);
+
+        // SE mail inserita da utente è in elenco mail registrate
+        if (miaMail === mailRegistrate[i]) {
+            esito = "positivo"; // esito diventa POSITIVO
+        }
     }
-}
 
-// Stampo in console messaggio accesso/rifiuto
-if (esito === "negativo") { // SE esito rimane NEGATIVO (mail utente NON presente in lista)
-    console.log("Mi dispiace, ma non sei sulla lista D:");
-} else if (esito === "positivo") { // SE esito è POSITIVO (mail utente presente in lista)
-    console.log("Puoi entrare :D");
+    // Creo messaggio output di accesso/rifiuto
+    if (esito === "negativo") { // SE esito rimane NEGATIVO (mail utente NON presente in lista)
+        messaggioOutput = "Mi dispiace, ma non sei sulla lista D:";
+    } else if (esito === "positivo") { // SE esito è POSITIVO (mail utente presente in lista)
+        messaggioOutput = "Puoi entrare :D";
+        esito = "negativo";  // per resettare esito dopo output
+    }
+
+    // Stampo messaggio output di accesso/rifiuto
+    console.log(messaggioOutput); // in console
+
+    document.getElementById("mostroEsito").innerHTML = messaggioOutput; // su pagina
+
 }
